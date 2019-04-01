2649a98cae4a4f5c91363bc61c895a677bb23061
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-01 20:30:03 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,7 @@
 def reformat_languages(languages)
   new_hash = {}
-  lang_hash.each do |lang_type, lang_data|
-    lang_hash.each do |name, attributes|
+  languages.each do |lang_type, lang_data|
+    languages.each do |name, attributes|
     new_hash[name] ||= attributes
     new_hash[name][:style] ||= []
     new_hash[name][:style] << lang_type
